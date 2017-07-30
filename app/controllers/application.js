51f23543fe1c919cbcf6fc632d4f8b851ecf7368
wolodarsky <tysonsafavi@me.com>
2017-07-30 12:27:28 -0600

    navigation simplification

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,5 +5,21 @@ export default Ember.Controller.extend({
   init() {
     this._super(...arguments);
     csv('vehicles-trim.csv').get( r => this.set("data", r));
+  },
+
+  makeOptions: Ember.computed("data", function() {
+    let data = this.get("data");
+
+    if (data) {
+      return data.uniqBy("make").map(c => c.make);
+    } else {
+      return [];
+    }
+  }),
+
+  actions: {
+    updateMake(e) {
+      //this.set("make", e.target.value);
+    }
   }
 });
